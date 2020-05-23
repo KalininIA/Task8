69935230cee9449041129c3f88bf036f759ead6f
KalininIA <snd64@bk.ru>
2020-05-23 18:05:44 +0300

    Task8 commit

---
+++ src/Task8.java
@@ -0,0 +1,9 @@
+public class Task8 {
+    public static void main(String args[]) {
+        int number = 1;
+        while (number <51) {
+            System.out.print(" " + number);
+            number++;
+        }
+    }
+}
